b8f585cc5431f3aa2c813b6dfa71078142749e1a
Prateek Gianchandani <prateek.searchingeye@gmail.com>
2018-04-12 11:30:05 +0400

    Added juniper fund, bug fixes

--- DVIA-v2/DVIA-v2/Vulnerabilities/Runtime Manipulation/Controller/RuntimeManipulationViewController.swift
+++ DVIA-v2/DVIA-v2/Vulnerabilities/Runtime Manipulation/Controller/RuntimeManipulationViewController.swift
@@ -1,10 +1,18 @@
 //
 //  RuntimeManipulationViewController.swift
-//  DVIA
-//
+//  DVIA - Damn Vulnerable iOS App (damnvulnerableiosapp.com)
 //  Created by Prateek Gianchandani on 23/11/17.
-//  Copyright © 2017 HighAltitudeHacks. All rights reserved.
-//
+//  Copyright © 2018 HighAltitudeHacks. All rights reserved.
+//  You are free to use this app for commercial or non-commercial purposes
+//  You are also allowed to use this in trainings
+//  However, if you benefit from this project and want to make a contribution, please consider making a donation to The Juniper Fund (www.thejuniperfund.org/)
+//  The Juniper fund is focusing on Nepali workers involved with climbing and expedition support in the high mountains of Nepal. When a high altitude worker has an accident (death or debilitating injury), the impact to the family is huge. The juniper fund provides funds to the affected families and help them set up a sustainable business.
+//  For more information,  visit www.thejuniperfund.org
+//  Or watch this video https://www.youtube.com/watch?v=HsV6jaA5J2I
+//  And this https://www.youtube.com/watch?v=6dHXcoF590E
+//  Let's make the world a better place for everyone :-)
+
+
 
 import UIKit
 
