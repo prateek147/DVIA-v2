488d2e80504ff1905445ebc8742834b078b41177
Prateek Gianchandani <prateek.searchingeye@gmail.com>
2018-04-20 16:09:02 +0400

    Changed header

--- DVIA-v2/DVIA-v2/Vulnerabilities/Attacking Third Party Libraries/Controller/FlurryLeakDetailsViewController.swift
+++ DVIA-v2/DVIA-v2/Vulnerabilities/Attacking Third Party Libraries/Controller/FlurryLeakDetailsViewController.swift
@@ -1,10 +1,15 @@
 //
 //  FlurryLeakDetailsViewController.swift
-//  DVIA-v2
-//
+//  DVIA - Damn Vulnerable iOS App (damnvulnerableiosapp.com)
 //  Created by Prateek Gianchandani on 4/16/18.
 //  Copyright © 2018 HighAltitudeHacks. All rights reserved.
-//
+//  You are free to use this app for commercial or non-commercial purposes
+//  You are also allowed to use this in trainings
+//  However, if you benefit from this project and want to make a contribution, please consider making a donation to The Juniper Fund (www.thejuniperfund.org/)
+//  The Juniper fund is focusing on Nepali workers involved with climbing and expedition support in the high mountains of Nepal. When a high altitude worker has an accident (death or debilitating injury), the impact to the family is huge. The juniper fund provides funds to the affected families and help them set up a sustainable business.
+//  For more information,  visit www.thejuniperfund.org
+//  Or watch this video https://www.youtube.com/watch?v=HsV6jaA5J2I
+//  And this https://www.youtube.com/watch?v=6dHXcoF590E
 
 import UIKit
 import Flurry_iOS_SDK
@@ -27,12 +32,12 @@ class FlurryLeakDetailsViewController: UIViewController, UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         //Device UDID for Flurry
         let identifierForVendor = UIDevice.current.identifierForVendor?.uuidString
-        //Shouldn't be sending the whole UDID
-        let strippedudid = identifierForVendor?.dropLast(10)
+        //Shouldn't be sending the whole UUID
+        let strippeduuid = identifierForVendor?.dropLast(10)
         textField.resignFirstResponder()
         let params: Dictionary = [
             "phone": textField.text,
-            "udid" : strippedudid
+            "udid" : strippeduuid
             ] as [String : Any]
         Flurry.logEvent("PhoneEntered", withParameters:params as Any as! [AnyHashable : Any])
                 return true
